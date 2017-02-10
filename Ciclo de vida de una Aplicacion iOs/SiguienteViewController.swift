4c9c2550a29711a09b45fe7c55ba3d5874f47655
alumno <alumno@a51-02.local>
2017-02-09 19:57:31 -0500

    Ciclo de una aplicación

---
+++ Ciclo de vida de una Aplicacion iOs/SiguienteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SiguienteViewController.swift
+//  Ciclo de vida de una Aplicacion iOs
+//
+//  Created by alumno on 9/02/17.
+//  Copyright © 2017 alumno. All rights reserved.
+//
+
+import UIKit
+
+class SiguienteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Ciclo de vida de una Aplicacion iOs/ViewController.swift
+++ Ciclo de vida de una Aplicacion iOs/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numero = 0
+    @IBOutlet weak var lblNumero: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("view Controller viewDidLoad")
+        
+        lblNumero.text="\(numero)"
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("view Controller viewDidAppear")
+        
+        numero = numero + 1
+        
+        lblNumero.text="\(numero)"
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("view Controller viewWillAppear")
+        
+        numero = numero + 1
+        lblNumero.text="\(numero)"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("view Controller viewWillDisappear")
+        
+        numero = numero + 1
+        lblNumero.text="\(numero)"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
